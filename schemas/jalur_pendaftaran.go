ffb9b14368b80a3b9d9d5a4a3519a6bb76667b0c
indrariksa <indrariksa@gmail.com>
2023-10-25 15:45:19 +0700

    feat(jalur_pendaftaran):tambah struck Jalur Pendaftaran

---
+++ schemas/jalur_pendaftaran.go
@@ -0,0 +1,14 @@
+package schemas
+
+type JalurPendaftaran struct {
+	IDJalur         int32  `gorm:"column:id_jalur;primaryKey;autoIncrement:true" json:"id_jalur"`
+	Jalur           string `gorm:"column:jalur" json:"jalur"`
+	NamaJalur       string `gorm:"column:nama_jalur" json:"nama_jalur"`
+	KeteranganJalur string `gorm:"column:keterangan_jalur" json:"keterangan_jalur"`
+	Status          string `gorm:"column:status;default:nonaktif" json:"status"`
+}
+
+// TableName JalurPendaftaran's table name
+func (*JalurPendaftaran) TableName() string {
+	return "jalur_pendaftaran"
+}
