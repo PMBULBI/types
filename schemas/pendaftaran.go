9183d8d77b032f1c35df260e77457218eb844f63
indrariksa <indrariksa@gmail.com>
2023-10-26 15:00:35 +0700

    fix(pendaftaran):perbaikan null string pada struck Pendaftaran

--- schemas/pendaftaran.go
+++ schemas/pendaftaran.go
@@ -1,19 +1,22 @@
 package schemas
 
-import "github.com/golang-module/carbon/v2"
+import (
+	"database/sql"
+	"github.com/golang-module/carbon/v2"
+)
 
 type Pendaftaran struct {
-	ID              int           `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
-	NamaMhs         string        `gorm:"column:nama_mhs;not null" json:"nama_mhs"`
-	AsalSekolah     string        `gorm:"column:asal_sekolah;not null" json:"asal_sekolah"`
-	EmailMhs        string        `gorm:"column:email_mhs;not null" json:"email_mhs"`
-	HpMhs           string        `gorm:"column:hp_mhs;not null" json:"hp_mhs"`
-	ProvinsiSekolah string        `gorm:"column:provinsi_sekolah" json:"provinsi_sekolah"`
-	KotaSekolah     string        `gorm:"column:kota_sekolah" json:"kota_sekolah"`
-	Password        string        `gorm:"column:password;not null" json:"password"`
-	StatusMhs       int           `gorm:"column:status_mhs;not null;default:0" json:"status_mhs"`
-	UsernameAdmin   string        `gorm:"column:username_admin" json:"username_admin"`
-	TglDaftarMhs    carbon.Carbon `gorm:"column:tgl_daftar_mhs;not null" json:"tgl_daftar_mhs"`
+	ID              int            `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
+	NamaMhs         string         `gorm:"column:nama_mhs;not null" json:"nama_mhs"`
+	AsalSekolah     string         `gorm:"column:asal_sekolah;not null" json:"asal_sekolah"`
+	EmailMhs        string         `gorm:"column:email_mhs;not null" json:"email_mhs"`
+	HpMhs           string         `gorm:"column:hp_mhs;not null" json:"hp_mhs"`
+	ProvinsiSekolah sql.NullString `gorm:"column:provinsi_sekolah" json:"provinsi_sekolah"`
+	KotaSekolah     sql.NullString `gorm:"column:kota_sekolah" json:"kota_sekolah"`
+	Password        string         `gorm:"column:password;not null" json:"password"`
+	StatusMhs       int            `gorm:"column:status_mhs;not null;default:0" json:"status_mhs"`
+	UsernameAdmin   sql.NullString `gorm:"column:username_admin" json:"username_admin"`
+	TglDaftarMhs    carbon.Carbon  `gorm:"column:tgl_daftar_mhs;not null" json:"tgl_daftar_mhs"`
 }
 
 func (*Pendaftaran) TableName() string {
