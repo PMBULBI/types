6981aebc63afb64492acf3026ae614e860a451fa
indrariksa <indrariksa@gmail.com>
2023-10-27 09:30:48 +0700

    feat(kotaresponse):tambah response kota

--- schemas/response.go
+++ schemas/response.go
@@ -4,3 +4,9 @@ type ProvinsiResponse struct {
 	IDProvinsi   string `json:"id_provinsi"`
 	NamaProvinsi string `json:"nama_provinsi"`
 }
+
+type KotaRequest struct {
+	IDKabupaten   string `gorm:"column:id_kabupaten;primaryKey" json:"id_kabupaten"`
+	IDProvinsi    string `gorm:"column:id_provinsi" json:"id_provinsi"`
+	NamaKabupaten string `gorm:"column:nama_kabupaten" json:"nama_kabupaten"`
+}
