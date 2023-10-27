14d0b27adc0ff94b425674916bfe53990218389d
indrariksa <indrariksa@gmail.com>
2023-10-27 09:40:45 +0700

    fix(kotaresponse):perbaikan response kota

--- schemas/response.go
+++ schemas/response.go
@@ -5,8 +5,8 @@ type ProvinsiResponse struct {
 	NamaProvinsi string `json:"nama_provinsi"`
 }
 
-type KotaRequest struct {
-	IDKabupaten   string `gorm:"column:id_kabupaten;primaryKey" json:"id_kabupaten"`
-	IDProvinsi    string `gorm:"column:id_provinsi" json:"id_provinsi"`
-	NamaKabupaten string `gorm:"column:nama_kabupaten" json:"nama_kabupaten"`
+type KotaResponse struct {
+	IDKabupaten   string `json:"id_kabupaten"`
+	IDProvinsi    string `json:"id_provinsi"`
+	NamaKabupaten string `json:"nama_kabupaten"`
 }
