ba97662e6c986f256cfcdc4fdfa2068d883f61e1
indrariksa <indrariksa@gmail.com>
2023-11-02 14:41:29 +0700

    struct(pendaftaran): response untuk pendaftaran

--- schemas/response.go
+++ schemas/response.go
@@ -1,5 +1,9 @@
 package schemas
 
+import (
+	"github.com/golang-module/carbon/v2"
+)
+
 type ProvinsiResponse struct {
 	IDProvinsi   string `json:"id_provinsi"`
 	NamaProvinsi string `json:"nama_provinsi"`
@@ -13,4 +17,18 @@ type KotaResponse struct {
 
 type ResponseLogin struct {
 	Email string `json:"email_mhs"`
+}
+
+type ResponsePendaftaran struct {
+	ID              int           `json:"id"`
+	NamaMhs         string        `json:"nama_mhs"`
+	AsalSekolah     string        `json:"asal_sekolah"`
+	EmailMhs        string        `json:"email_mhs"`
+	HpMhs           string        `json:"hp_mhs"`
+	ProvinsiSekolah string        `json:"provinsi_sekolah"`
+	KotaSekolah     string        `json:"kota_sekolah"`
+	Password        string        `json:"password"`
+	StatusMhs       int           `json:"status_mhs"`
+	UsernameAdmin   string        `json:"username_admin"`
+	TglDaftarMhs    carbon.Carbon `json:"tgl_daftar_mhs"`
 }
