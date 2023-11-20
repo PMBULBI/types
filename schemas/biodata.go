bb14ad46b2f9f76ab306c7d983b14f1775d692f4
indrariksa <indrariksa@gmail.com>
2023-11-20 10:24:46 +0700

    struct(BiodataProdi): tambah struct BiodataProdi

--- schemas/biodata.go
+++ schemas/biodata.go
@@ -1,12 +1,23 @@
 package schemas
 
 type BiodataJalur struct {
-	IdHash     string `gorm:"column:id_hash;primaryKey;autoIncrement:true" json:"id_hash"`
+	IdHash     string `gorm:"column:id_hash;primaryKey" json:"id_hash"`
 	IDJalur    int    `gorm:"column:id_jalur" json:"id_jalur"`
 	TahunLulus int    `gorm:"column:tahun_lulus;not null" json:"tahun_lulus"`
 	KodeRef    string `gorm:"column:kode_ref;not null" json:"kode_ref"`
 }
 
+type BiodataProdi struct {
+	IdHash     string `gorm:"column:id_hash;primaryKey" json:"id_hash"`
+	IDFakultas int    `gorm:"column:id_fakultas" json:"id_fakultas"`
+	Prodi1     int    `gorm:"column:prodi1;not null" json:"prodi1"`
+	Prodi2     int    `gorm:"column:prodi2;not null" json:"prodi2"`
+}
+
 func (*BiodataJalur) TableName() string {
 	return "biodata_jalur"
+}
+
+func (*BiodataProdi) TableName() string {
+	return "biodata_prodi"
 }
