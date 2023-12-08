66a37390e698a8fe514a252f73c1af90c3a0902a
indrariksa <indrariksa@gmail.com>
2023-12-08 15:06:59 +0700

    fix(program_studi): edit tipe data kode prodi

--- schemas/program_studi.go
+++ schemas/program_studi.go
@@ -3,7 +3,7 @@ package schemas
 type ProgramStudi struct {
 	ID               int    `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
 	Fakultas         int    `gorm:"column:fakultas" json:"fakultas"`
-	KodeProgramStudi string `gorm:"column:kode_program_studi" json:"kode_program_studi"`
+	KodeProgramStudi int    `gorm:"column:kode_program_studi" json:"kode_program_studi"`
 	ProgramStudi     string `gorm:"column:program_studi" json:"program_studi"`
 }
 
