ed809bd38445c058c5e95af4b0773037bd12e9cb
indrariksa <indrariksa@gmail.com>
2023-12-08 15:06:39 +0700

    struct(jalur_prodi): buat struct join JalurProdi

--- schemas/jalur_prodi.go
+++ schemas/jalur_prodi.go
@@ -6,6 +6,13 @@ type JalurProdi struct {
 	ProgramStudi int    `gorm:"column:prodi" json:"prodi"`
 }
 
+type JalurProdiWithProdi struct {
+	ID               int    `gorm:"column:id;primaryKey" json:"id"`
+	Jalur            string `gorm:"column:jalur" json:"jalur"`
+	KodeProgramStudi int    `gorm:"column:kode_prodi" json:"kode_prodi"`
+	ProgramStudi     int    `gorm:"column:prodi" json:"prodi"`
+}
+
 func (*JalurProdi) TableName() string {
 	return "jalur_prodi"
 }
