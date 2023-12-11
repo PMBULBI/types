a0f73c0367fcf8942c4170933785985102542641
indrariksa <indrariksa@gmail.com>
2023-12-11 14:57:54 +0700

    struct(jalur_tahun): tambah struct JalurTahunJoin

--- schemas/jalur_tahun.go
+++ schemas/jalur_tahun.go
@@ -6,6 +6,13 @@ type JalurTahun struct {
 	Tahun int    `gorm:"column:tahun" json:"tahun"`
 }
 
+type JalurTahunJoin struct {
+	ID        int    `gorm:"column:id;primaryKey" json:"id"`
+	Jalur     string `gorm:"column:jalur" json:"jalur"`
+	NamaJalur string `gorm:"column:nama_jalur" json:"nama_jalur"`
+	Tahun     int    `gorm:"column:tahun" json:"tahun"`
+}
+
 func (*JalurTahun) TableName() string {
 	return "jalur_tahun"
 }
