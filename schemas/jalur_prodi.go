4fad1a06da222531f761270cf272576b845007d0
indrariksa <indrariksa@gmail.com>
2023-12-12 09:39:12 +0700

    struct(jalur_prodi): edit kolom jalur menjadi idjalur

--- schemas/jalur_prodi.go
+++ schemas/jalur_prodi.go
@@ -1,14 +1,14 @@
 package schemas
 
 type JalurProdi struct {
-	ID           int    `gorm:"column:id;primaryKey" json:"id"`
-	Jalur        string `gorm:"column:jalur" json:"jalur"`
-	ProgramStudi int    `gorm:"column:prodi" json:"prodi"`
+	ID           int `gorm:"column:id;primaryKey" json:"id"`
+	IDJalur      int `gorm:"column:id_jalur" json:"id_jalur"`
+	ProgramStudi int `gorm:"column:prodi" json:"prodi"`
 }
 
 type JalurProdiWithProdi struct {
 	ID               int    `gorm:"column:id;primaryKey" json:"id"`
-	Jalur            string `gorm:"column:jalur" json:"jalur"`
+	IDJalur          int    `gorm:"column:id_jalur" json:"id_jalur"`
 	KodeProgramStudi int    `gorm:"column:prodi" json:"prodi"`
 	ProgramStudi     string `gorm:"column:program_studi" json:"program_studi"`
 }
