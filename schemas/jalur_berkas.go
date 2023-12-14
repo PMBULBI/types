11844ee4f667c3bb8eefbcc3c76c481e43562881
indrariksa <indrariksa@gmail.com>
2023-12-14 10:12:09 +0700

    struct(jalur_berkas): tambah struct join

--- schemas/jalur_berkas.go
+++ schemas/jalur_berkas.go
@@ -1,9 +1,16 @@
 package schemas
 
 type JalurBerkas struct {
-	ID     int    `gorm:"column:id;primaryKey" json:"id"`
-	Jalur  int    `gorm:"column:id_jalur" json:"id_jalur"`
-	Berkas string `gorm:"column:berkas" json:"berkas"`
+	ID      int    `gorm:"column:id;primaryKey" json:"id"`
+	IDJalur int    `gorm:"column:id_jalur" json:"id_jalur"`
+	Berkas  string `gorm:"column:berkas" json:"berkas"`
+}
+
+type JalurBerkasJoin struct {
+	ID        int    `gorm:"column:id;primaryKey" json:"id"`
+	IDJalur   int    `gorm:"column:id_jalur" json:"id_jalur"`
+	NamaJalur string `gorm:"column:nama_jalur" json:"nama_jalur"`
+	Berkas    string `gorm:"column:berkas" json:"berkas"`
 }
 
 func (*JalurBerkas) TableName() string {
